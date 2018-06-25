7a19bd4193c411bf68a5e65c3166fe5b9e7be2d0
Folbort, Ioness-heorgX <ioness-heorgx.folbort@intel.com>
2018-06-25 19:40:01 +0300

    optimise multiples up to constant evaluation

--- euler-project/Multiples/main.cpp
+++ euler-project/Multiples/main.cpp
@@ -9,16 +9,18 @@ int get_sum_of(int divisor, int upper_bound)
 	int last = upper_bound - upper_bound%divisor;
 	int first = divisor;
 
-	return (last - first)*n/2;
+	return ((last + first)*n)/2;
 
 }
 
 int main(int argc, char** argv){
 
-	int sum = 0;
+	unsigned long long int sum = 0;
 	string inp_val = argv[1];
 	int n = stoi(inp_val);
 
-	sum = get_sum_of(3,n) + get_sum_of(5,n) - get_sum_of(15,n);
-	cout << sum;
+	sum += get_sum_of(3,n) + get_sum_of(5,n) - get_sum_of(15,n);
+
+	cout << sum << "\n";
+
 }
