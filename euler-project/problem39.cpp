b24ac0dabd0b18b3afe15e0e8214655eaeaba965
IohannesFolbort <petrarce@gmail.com>
2024-06-25 23:06:42 +0200

    problem39

--- euler-project/problem39.cpp
+++ euler-project/problem39.cpp
@@ -1,53 +1,38 @@
 #include <iostream>
 #include <absl/strings/str_format.h>
-float Hipotenusa(int P /*Perimeter*/, int a /*smaller cater*/)
-{
-
-}
-
-float LargerCatet(int P /*Perimeter*/, int a /*smaller cater*/)
-{
-
-}
-
-bool IsLargerCatetInt(int P, int a)
-{
-
-}
-
-bool IsHipotenusaInt(int P, int a)
-{
-
-}
-
-int SolutionCount(int P /*Perimeter*/)
-{
-    int solCount = 0;
-    for(int a = 1; a < P / 2 && a <= LargerCatet(P, a); a++)
-    {
-        if (!IsLargerCatetInt(P, a) || IsHipotenusaInt(P, a))
-            continue;
-
-        solCount++;
-        auto h = Hipotenusa(P, a);
-        auto b = LargerCatet(P, a);
-        std::cout << absl::StrFormat("P(%u) solution: {%i, %f, %f}", P, a, b, h);
-    }
-    return solCount;
-}
 
 int main(int ac, char** av)
 {
-    int maxP = std::stoi(av[1]);
-    std::pair<int, int> PvsSolCnt = {0, 0};
-    for (int P = 3; P <= maxP; P++) {
+    size_t maxP = std::stoi(av[1]);
+    std::pair<size_t, size_t> PvsSolCnt = {0, 0};
+    // integer solution are oby available for even P's
+    for (size_t P = 2; P <= maxP; P += 2) {
 
-        auto sc = SolutionCount(P);
-        PvsSolCnt = std::max(PvsSolCnt, {P, sc}, [](const auto &left, const auto& right) {return left.second > right.second; } );
-        std::cout << absl::StrFormat("P(%u) solutions: %i", P, sc);
+        auto sc = [&](){
+            size_t solCount = 0;
+            // larger catet of the right triangle
+            auto b = [](float P /*Perimeter*/, float a /*smaller cater*/){ return (P * P - 2 * a * P) / (2.f * ( P - a)); };
+
+            for(size_t a = 1; a < P / 2 && a <= b(P, a); a++)
+            {
+                // since P is even it is enough that a * a is divisible by (P - a)
+                // for hipotenuse and larger catet to have an integer value
+                if ((a * a) % (P - a) != 0)
+                    continue;
+
+                solCount++;
+            }
+            return solCount;
+        }();
+        PvsSolCnt = std::max(
+            PvsSolCnt,
+            {P, sc},
+            [](const auto &left,
+               const auto& right) { return left.second <= right.second; }
+        );
     }
 
-    std::cout << absl::StrFormat("max solutions: P(%i) : %i", PvsSolCnt.first, PvsSolCnt.second);
+    std::cout << absl::StrFormat("max solutions: P(%i) : %i\n", PvsSolCnt.first, PvsSolCnt.second);
 
     return 0;
 }
